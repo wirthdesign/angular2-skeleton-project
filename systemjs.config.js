3b9d94d13e0c0b3221963699da1ad96643a804a1
Kevin Wirth <me@wirthdesign.com>
2016-11-26 20:27:23 -0500

    Fixed typos in systemjs config

--- systemjs.config.js
+++ systemjs.config.js
@@ -10,9 +10,9 @@
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umg.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/router/bundles/forms.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
       'rxjs': 'npm:rxjs',
       'angular-in-memoery-web-api': 'npm:angular-in-memoery-web-api'
